d5539de564a98a18e85dd37764fcd66d99325731
Ben <me@benspace.xyz>
2025-01-10 20:59:17 +0700

    fix: Remove IsNotEmpty validation from UpdateUserDto

--- src/core/users/dto/updateUser.dto.ts
+++ src/core/users/dto/updateUser.dto.ts
@@ -5,13 +5,11 @@ import { IsEnum, IsNotEmpty, IsOptional, IsString } from "class-validator";
 export class UpdateUserDto {
   @IsOptional()
   @IsString()
-  @IsNotEmpty()
   @ApiPropertyOptional()
   username?: string;
 
   @IsOptional()
   @IsString()
-  @IsNotEmpty()
   @ApiPropertyOptional()
   password?: string;
 
