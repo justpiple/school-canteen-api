534875dbc574a26c66d8ae1358e54e20f4ed048a
Ben <me@benspace.xyz>
2025-05-02 14:38:44 +0700

    feat: Update app.listen to use process.env.PORT

--- src/main.ts
+++ src/main.ts
@@ -45,7 +45,7 @@ async function bootstrap() {
     swaggerOptions: { persistAuthorization: true },
   });
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap();
